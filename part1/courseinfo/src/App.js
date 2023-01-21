15fb7712a022f6bde381bf9f50caf3d0013bc157
Flumen6 <jakub.ogorkis@proton.me>
2023-01-21 14:21:40 +0100

    curseInfo3-5-done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,44 +1,55 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content parts={[part1, part2, part3]} exercises = {[exercises1, exercises2, exercises3]}/>
-      <Total total = {exercises1 + exercises2 + exercises3}></Total>
+      <Header course = {course.name}/>
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
     </div>
   )
 }
-const Header = (props) => {
+const Header = ({course}) => {
   return(
-    <h1>{props.course}</h1>
+    <h1>{course}</h1>
   )
 }
-const Content = ({parts, exercises}) => {
+const Content = ({parts}) => {
   return(
     <div>
-      <Part part={parts[0]} exercises = {exercises[0]}/>
-      <Part part={parts[1]} exercises = {exercises[1]}/>
-      <Part part={parts[2]} exercises = {exercises[2]}/>
+      <Part part={parts[0]}/>
+      <Part part={parts[1]}/>
+      <Part part={parts[2]}/>
     </div>
   )
 }
-const Part = (props) => {
+const Part = ({part}) => {
   return(
     <p>
-      {props.part} {props.exercises}
+      {part.name} {part.exercises}
     </p>
   )
 }
-const Total = (props) => {
+const Total = ({parts}) => {
   return(
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
   )
 }
 
